00fc1a47c36a1d89b1a8a32e27c47ba8ed488b91
appledonut-dev <peia3@uci.edu>
2026-02-03 23:38:40 +0000

    Update report.py

--- report.py
+++ report.py
@@ -9,9 +9,21 @@ from queue import Queue
 # 4. Subdomain count
 
 class Report(object):
-    # class variable
+    # class variable to handle multithreading
     report_queue = Queue()
 
     def __init__(self, worker_id):
         self.worker_id = worker_id
+        self.report = {}
+
+    def get_unique_pages(self, unique_pages: list):
+        self.report["unique"] = unique_pages
+
+    def get_longest_page(self, longest_page: str, word_count: int):
+        self.report["longest"] = (longest_page, word_count)
+
+    def run(self):
+        self.report{"worker_id"] = worker_id
+        Report.report_queue.put(self.report)
+        
 
