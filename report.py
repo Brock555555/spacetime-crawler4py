2ebdbc4fb72a02733f7e1cc7d2dab98e5f47ce5e
appledonut-dev <appledonutvs.applepie@gmail.com>
2026-02-03 17:43:30 -0800

    Update report.py

--- report.py
+++ report.py
@@ -1,6 +1,7 @@
 # Create a way collect data from each worker/scraper instance
 
 from queue import Queue
+from collections import defaultdict
 
 #--------------Report stuff-----------------------
 # 1. Unique Pages
@@ -8,22 +9,41 @@ from queue import Queue
 # 3. Most common words
 # 4. Subdomain count
 
-class Report(object):
+class Report():
+    WORD_COUNT = 50
     # class variable to handle multithreading
     report_queue = Queue()
+    word_bank = defaultdict()
 
     def __init__(self, worker_id):
         self.worker_id = worker_id
         self.report = {}
 
-    def get_unique_pages(self, unique_pages: list):
-        self.report["unique"] = unique_pages
+    def get_url(self, url: str):
+        self.report["url"] = url
 
-    def get_longest_page(self, longest_page: str, word_count: int):
-        self.report["longest"] = (longest_page, word_count)
+    def get_page_length(self, url: str, word_count: int):
+        self.report["length"] = (url, word_count)
 
-    def run(self):
+    def get_most_common_words(self, text):
+        common_words = defaultdict()
+        # tokenize???
+        # 50 most common words per page
+        for i in range(Report.WORD_COUNT):
+            if common_words:
+                most_common = max(my_dict, key=my_dict.get)
+                k, v = commonwords.pop(most_common)
+            else:
+                break
+        self.report["words"] = common_words
+
+    def send_report(self):
         self.report{"worker_id"] = worker_id
         Report.report_queue.put(self.report)
-        
+
+    @classmethod
+    def aggregate_reports(cls):
+        while not Report.report_queue.empty():
+            report = Report.report_queue.get()
+            # unpack report and process
 
