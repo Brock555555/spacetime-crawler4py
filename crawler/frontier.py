4d91b3184ba626f9de6c0331ab7ab2f589554ea7
“Brock <bballan@uci.edu>
2026-02-04 03:46:25 +0000

    Replacing lock 3.13 changes

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -12,7 +12,7 @@ class Frontier(object):
         self.logger = get_logger("FRONTIER")
         self.config = config
         self.to_be_downloaded = list()
-        self.save_lock = RLock()
+        
         if not os.path.exists(self.config.save_file) and not restart:
             # Save file does not exist, but request to load save.
             self.logger.info(
@@ -31,25 +31,21 @@ class Frontier(object):
         else:
             # Set the frontier state with contents of save file.
             self._parse_save_file()
-            with self.save_lock:
-                empty = not bool(self.save)
-
-            if empty:
+            if not self.save:
                 for url in self.config.seed_urls:
                     self.add_url(url)
 
     def _parse_save_file(self):
         ''' This function can be overridden for alternate saving techniques. '''
-        with self.save_lock:
-            total_count = len(self.save)
-            tbd_count = 0
-            for url, completed in self.save.values():
-                if not completed and is_valid(url):
-                    self.to_be_downloaded.append(url)
-                    tbd_count += 1
+        total_count = len(self.save)
+        tbd_count = 0
+        for url, completed in self.save.values():
+            if not completed and is_valid(url):
+                self.to_be_downloaded.append(url)
+                tbd_count += 1
         self.logger.info(
-                f"Found {tbd_count} urls to be downloaded from {total_count} "
-                f"total urls discovered.")
+            f"Found {tbd_count} urls to be downloaded from {total_count} "
+            f"total urls discovered.")
 
     def get_tbd_url(self):
         try:
@@ -60,19 +56,17 @@ class Frontier(object):
     def add_url(self, url):
         url = normalize(url)
         urlhash = get_urlhash(url)
-        with self.save_lock:
-            with shelve.open(self.config.save_file) as save:
-                if urlhash not in save:
-                    save[urlhash] = (url, False)
-                    save.sync()
-                    self.to_be_downloaded.append(url)
+        if urlhash not in self.save:
+            self.save[urlhash] = (url, False)
+            self.save.sync()
+            self.to_be_downloaded.append(url)
     
     def mark_url_complete(self, url):
         urlhash = get_urlhash(url)
-        with self.save_lock:
-            with shelve.open(self.config.save_file) as save:
-                if urlhash not in save:
-                    self.logger.error(
-                        f"Completed url {url}, but have not seen it before.")
-                save[urlhash] = (url, True)
-                save.sync()
+        if urlhash not in self.save:
+            # This should not happen.
+            self.logger.error(
+                f"Completed url {url}, but have not seen it before.")
+
+        self.save[urlhash] = (url, True)
+        self.save.sync()
