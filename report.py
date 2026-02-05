73377aed11e9caa3ad646cc35ec4af97bef5ac8d
appledonut-dev <peia3@uci.edu>
2026-02-05 21:15:16 +0000

    Added page length to report.py

--- report.py
+++ report.py
@@ -1,6 +1,8 @@
 # Create a way collect data from each worker/scraper instance
 
 from queue import Queue
+from bs4 import BeautifulSoup, Comment
+import re
 
 #--------------Report stuff-----------------------
 # 1. Unique Pages
@@ -12,18 +14,32 @@ class Report(object):
     # class variable to handle multithreading
     report_queue = Queue()
 
-    def __init__(self, worker_id):
-        self.worker_id = worker_id
-        self.report = {}
+    def __init__(self, worker_id: int, url, soup: BeautifulSoup):
+        self.report = {"worker_id": worker_id, "url": url}
+        # WIP remove fragments from url
+        self.soup = soup
 
-    def get_unique_pages(self, unique_pages: list):
-        self.report["unique"] = unique_pages
+    def set_page_length(self, url: str, word_count: int):
+        self.report["length"] = (url, word_count)
 
-    def get_longest_page(self, longest_page: str, word_count: int):
-        self.report["longest"] = (longest_page, word_count)
+    def is_comment(self, element):
+        return isinstance(element, Comment)
 
-    def run(self):
-        self.report{"worker_id"] = worker_id
+    def get_words(self, soup: BeautifulSoup):
+        # THIS IS DESTRUCTIVE SO IT HAS TO BE RUN AFTER EVERYTHING ELSE
+        # filter out tags and comments
+        for tag in soup(["script", "style", "head", "title", "meta", "[document]"]):
+            tag.decompose()
+        for comment in soup.find_all(string = self.is_comment):
+            comment.decompose()
+        # finally gets text
+        text = soup.get_text(separator=' ', strip=True)
+        # tokenize by letters and apostrophes only
+        words = re.findall(r"[a-zA-Z]+(?:'[a-zA-Z]+)*", text.lower())
+
+    def run(self, worker_id: int, url: str, soup: BeautifulSoup):
+        self.words = self.get_words(rep.soup)
+        self.set_page_length(url, len(self.words))
         Report.report_queue.put(self.report)
         
 
