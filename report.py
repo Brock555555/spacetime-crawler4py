36250f5198e7d3bd197977835b4073ce20ff6c12
Albert Pei <peia3@uci.edu>
2026-02-06 17:21:47 -0800

    Updated report.py
    Stop words are removed hopefully
    Prints correctly to file hopefully

--- report.py
+++ report.py
@@ -19,9 +19,8 @@ class Report:
     report_queue = Queue()
     unique_pages = set()
     subdomain_page_count = defaultdict(int)
-    sorted_subdomains = dict()
-    max_length = -1
-    max_page = str()
+    longest_length = -1
+    longest_page = str()
     combined_word_frequencies = defaultdict(int)
     top_50_words = list(())
 
@@ -58,6 +57,10 @@ class Report:
         frequencies = defaultdict(int)
         for word in words:
             frequencies[word] += 1
+        # Don't report stop words
+        for word in frequencies:
+            if word in STOP_WORDS:
+                frequencies.pop(word)
         self.report["word frequencies"] = frequencies
 
     def run(self):
@@ -77,9 +80,9 @@ class Report:
             # Add url to set and then count the length of the set
             Report.unique_pages.add(report["url"])
             # Update url of longest page length
-            if report["page length"] > Report.max_length:
-                Report.max_length = report["page length"]
-                Report.max_page = report["url"]
+            if report["page length"] > Report.longest_length:
+                Report.longest_length = report["page length"]
+                Report.longest_page = report["url"]
             # Add/update word frequencies
             for word, frequency in report["word frequencies"].items():
                 Report.combined_word_frequencies[word] += frequency
@@ -91,10 +94,197 @@ class Report:
         for page in Report.unique_pages:
             Report.subdomain_page_count[parse.urlparse(page).hostname] += 1
         # sort them alphabetically into a new dict
-        Report.sorted_subdomains = dict(sorted(Report.subdomain_page_count.items()))
+        Report.subdomain_page_count = dict(sorted(Report.subdomain_page_count.items()))
 
     @classmethod
     def write_results(cls, file_name = "report.txt"):
         with open(file_name, 'w') as out_file:
-            out_file.write(f"Number of pages found: {len(Report.unique_pages)}")
+            out_file.write(f"Number of unique pages found: {len(Report.unique_pages)}\n")
+            out_file.write(f"Longest page in terms of word count: {Report.longest_page}\n")
+            out_file.write("50 most common words (ignoring stop words):\n")
+            print(*Report.top_50_words[0:10], sep=", ", file = out_file)
+            print(*Report.top_50_words[10:20], sep=", ", file = out_file)
+            print(*Report.top_50_words[20:30], sep=", ", file = out_file)
+            print(*Report.top_50_words[30:40], sep=", ", file = out_file)
+            print(*Report.top_50_words[40:50], sep=", ", file = out_file)
+            out_file.write(f"Number of subdomains: {len(Report.subdomain_page_count)}\n")
+            out_file.write("Subdomain page count:\n")
+            for subdomain, page_count in Report.subdomain_page_count:
+                out_file.write(f"{subdomain}\t{page_count}\n")
 
+STOP_WORDS = set("""
+a
+about
+above
+after
+again
+against
+all
+am
+an
+and
+any
+are
+aren't
+as
+at
+be
+because
+been
+before
+being
+below
+between
+both
+but
+by
+can't
+cannot
+could
+couldn't
+did
+didn't
+do
+does
+doesn't
+doing
+don't
+down
+during
+each
+few
+for
+from
+further
+had
+hadn't
+has
+hasn't
+have
+haven't
+having
+he
+he'd
+he'll
+he's
+her
+here
+here's
+hers
+herself
+him
+himself
+his
+how
+how's
+i
+i'd
+i'll
+i'm
+i've
+if
+in
+into
+is
+isn't
+it
+it's
+its
+itself
+let's
+me
+more
+most
+mustn't
+my
+myself
+no
+nor
+not
+of
+off
+on
+once
+only
+or
+other
+ought
+our
+ours
+ourselves
+out
+over
+own
+same
+shan't
+she
+she'd
+she'll
+she's
+should
+shouldn't
+so
+some
+such
+than
+that
+that's
+the
+their
+theirs
+them
+themselves
+then
+there
+there's
+these
+they
+they'd
+they'll
+they're
+they've
+this
+those
+through
+to
+too
+under
+until
+up
+very
+was
+wasn't
+we
+we'd
+we'll
+we're
+we've
+were
+weren't
+what
+what's
+when
+when's
+where
+where's
+which
+while
+who
+who's
+whom
+why
+why's
+with
+won't
+would
+wouldn't
+you
+you'd
+you'll
+you're
+you've
+your
+yours
+yourself
+yourselves
+""".split())
