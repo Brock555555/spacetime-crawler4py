bf32af0bdc792955da555444cccf0c92c80f89ee
“Brock <bballan@uci.edu>
2026-02-12 00:16:04 +0000

    Added comments to shared.py

--- shared.py
+++ shared.py
@@ -1,5 +1,5 @@
 from threading import Lock
 
-error_urls = set()      # shared global set of URLs that errored
-error_lock = Lock()     # optional lock for thread safety
-unique_urls = set()
+error_urls = set() # shared global set of URLs that errored
+error_lock = Lock() # optional lock for thread safety
+unique_urls = set() #shared global set of unique urls
