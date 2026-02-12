4e7425a7c7ecb78cfa03be286eb997a055f1b728
“Brock <bballan@uci.edu>
2026-02-12 00:15:11 +0000

    Added comments to Scraper for Explanation

--- scraper.py
+++ scraper.py
@@ -9,36 +9,40 @@ from hashlib import md5
 from report import Report
 from shared import error_urls, error_lock, unique_urls
 
-#------------------LIST OF THINGS LEFT TO DO-------------------------------- In order of importance
-# Finish Report so we can turn in our data
-# . Finalize large-file avoidance:
-#    - either add Content-Length heuristic
-#    - or document why extension + content filters suffice
-# 5. Handle Ctrl+C KeyboardException
+#Only thing that wasnt completed was the EC for near url
 
-#----------------Extra Credit stuff, to be done if we have time-----------------
-# 1. Implement exact and near webpage similarity detection
-# 2. Make the crawler multithreaded.
-
-#these two here are NOT thread safe
-unique_urls = set() # can still include for now? this currently prevents us from running duplicate urls
-error_urls = set() # will never add a url of this set again # TODO: Move this into worker (to make it persistent) and make sure worker doesn't use the error URLS as well (those that returned error status)
+#Technically the following two lines should have been removed since they redefine the global listrs, 
+# but due to checking in worker it still works, they ARE thread safe since they are only accessed with locks
+unique_urls = set() 
+error_urls = set()
 allowed_domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"]
 
 def scraper(url, resp, blacklist, whitelist, site_map):
-    #print(blacklist, whitelist, site_map)
-    links = extract_next_links(url, resp, site_map)
-    return [link for link in links if is_valid(link, blacklist, whitelist)]
+    """
+    Gets called by each individual worker, the worker provides:
+    Url: the url from the downloaded link from the frontier
+    resp: the response from the website when downloading
+    blacklist: a set of paths that robots.txt does not allow us to go into
+    whitelist: a set of allowed paths from disallowed paths in robots.txt
+    site_map: a set of xml links leading to sitemaps if they exist in robots.txt
+    """
+    links = extract_next_links(url, resp, site_map) #extracts links from site_maps and websites
+    return [link for link in links if is_valid(link, blacklist, whitelist)] #filters links to be added to the frontier
 
 def extract_next_links(url, resp, site_map):
-    # site_map, a set of more links from robots
-    XML = False
+    """
+    Gets called by scraper.
+    Url: the url from the downloaded link from the frontier
+    resp: the response from the website when downloading
+    site_map: a set of xml links leading to sitemaps if they exist in robots.txt
+    """
+    XML = False #changes the parsing type with soup if XML is detected
 
     #only process successful 200 OK responses
     if resp.status != 200 or not resp.raw_response or not resp.raw_response.content:#further error checking
         with error_lock:
-            error_urls.add(url)
-        return []
+            error_urls.add(url) #adds the url to the list of urls to never check again
+        return [] #there are no links to find here
 
     unique = urldefrag(url)[0]
 
@@ -53,14 +57,14 @@ def extract_next_links(url, resp, site_map):
             #just add the site_map links, it will get parsed when the frontier reaches it later
             for site_map_url in site_map:
                 clean = urldefrag(site_map_url)[0]
-                links.append(clean)
+                links.append(clean) #remove the fragment and add to the list
         
-        #parse raw bytes of page content into soup obj
+        #parse raw bytes of page content into soup obj, check if xml file from site_map, change parsing type if true
         if resp.url.endswith(".xml") or "application/xml" in resp.raw_response.headers.get("Content-Type", ""):
             soup = BeautifulSoup(resp.raw_response.content, "xml")
             XML = True
         else:
-            soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+            soup = BeautifulSoup(resp.raw_response.content, "html.parser") #parse normally
 
         #remove script and style elements from word count
         for ss in soup(["script", "style"]):
@@ -76,7 +80,7 @@ def extract_next_links(url, resp, site_map):
 
 
         if XML:
-            for loc in soup.find_all("loc"):
+            for loc in soup.find_all("loc"):#xml uses loc instead of href
                 links.append(loc.text)
         else:
             #look for all <a> tags with "href" to find out where to go next
@@ -87,7 +91,7 @@ def extract_next_links(url, resp, site_map):
                 base_url = urldefrag(absolute_url)[0]
                 links.append(base_url)
 
-        # Call report module.  Lmk if this is a suitable place to put this.
+        # Call report module. Which sends the soup object to report for processing
         report = Report(url, soup)
         report.run()
 
@@ -99,7 +103,11 @@ def extract_next_links(url, resp, site_map):
 
 
 def normalize_url(url: str) -> str:
-    """Remove ephemeral query parameters that don't change main page content."""
+    """
+    Remove ephemeral query parameters that don't change main page content.
+    Was used to help identify unique urls by parsing the query
+    Worker was updated so this is now Legacy code and isnt used elsewhere
+    """
     parsed = urlparse(url)
     qs = parse_qs(parsed.query)
 
@@ -119,11 +127,13 @@ def normalize_url(url: str) -> str:
     return urldefrag(normalized)[0]  # remove fragment
 
 def is_valid(url, blacklist, whitelist):
-    #blacklist: a set of paths the crawler is not allowed to go into
-    #whitelist: a set of paths the crawler is allowed into from disallowed paths
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
+    """
+    Called by scraper to find good links
+    blacklist: a set of paths the crawler is not allowed to go into
+    whitelist: a set of paths the crawler is allowed into from disallowed paths
+    Decide whether to crawl this url or not. 
+    If you decide to crawl it, return True; otherwise return False.
+    """
     try:
         with error_lock:
             if url in error_urls:
@@ -137,7 +147,7 @@ def is_valid(url, blacklist, whitelist):
         #from slides scheme://domain:port/path?query_string#fragment_id
         domain = parsed.hostname.lower() if parsed.hostname else "" #returns the entire domain, does not include the /before path, this does however include www.
         #domain should already be lowercase
-        
+        #checks if the domain ends with one of the specific domains, disallows suffix traps
         if not any(domain == d or domain.endswith("." + d) for d in allowed_domains):
             return False #its crude but gets it in around O(1) - O(n) for larger string domains
 
@@ -167,22 +177,23 @@ def is_valid(url, blacklist, whitelist):
         # Filter out timeline pages
         if "/timeline" in path:
             return False
-
+        #filter out pages that contain weird wiki pages (im looking at you epstein and grape) or version numbers
         if path.startswith("/wiki/") and "version=" in query:
             return False
-
+        #filter out any urls that are just images
         if any(x in path or x in query for x in ["image", "img", "photo", "gallery", "media"]):
             return False
-        #add ml or dataset check
+        #filter out data sets and machine learning paths
         dataset_keywords = ("/data/", "/dataset/", "/downloads/")
         if any(keyword in path for keyword in dataset_keywords):
             return False
-
+        #filter out cropped images, pictures, and epsteins wiki
         low_value_keywords = ("/wiki/", "/cropped", "~epstein/pix")
         if any(keyword in path for keyword in low_value_keywords):
             return False
 
-        #updated whitelist vs blacklist logic so that it will check specific paths
+        #updated whitelist vs blacklist logic so that it will check specific paths\
+        #instantly returns false if the link's path is in blacklist unless it allowed in whitelist
         for bl in blacklist:
             if path.startswith(bl):
                 if not any(path.startswith(w1) for w1 in whitelist):
@@ -206,15 +217,15 @@ def is_valid(url, blacklist, whitelist):
                 return False
         if len(url) > 200:
             return False #long urls usually traps
-        
+        #dont know who added this but I would assume it was done for a trap detection
         if domain.startswith("wiki."):
             return False
-
+        #filters out directories where we just have a set of page numbers
         if re.search(r"/page/\d+", path):
             page_num = int(re.search(r"/page/(\d+)", path).group(1))
             if page_num > 5:
                 return False
-
+        #more calender and events detection
         if (
             path.startswith("/events")
             or "/event/" in path
@@ -223,12 +234,12 @@ def is_valid(url, blacklist, whitelist):
         ):
             return False
         
-        if "doku.php" in path:
+        if "doku.php" in path: #avoids a script used in wiki docu series
             return False
-        
+        #filters out dynamic or non-content urls
         if re.search(r"do=media|tab_|image=|ical=|outlook-ical=|tribe_", query):
             return False
-            
+        #filters out older seminar series in favor of new updated ones
         if "seminar-series" in path and re.search(r"\d{4}-\d{4}", path):
             return False
 
@@ -237,7 +248,8 @@ def is_valid(url, blacklist, whitelist):
     
             
         #this is the file type checker, was in the project from the start - might need to be added to
-        #re added for xml
+        #readded for xml
+        #essentially it detects if the end of the url ends in some kind of file type such as scraper.py or project.c
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico|png|tiff?|svg|heic|webp|avif"
             r"|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|flac|aac|webm"
